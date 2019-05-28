0c1d0955660088e2adea520495dad0c2f33d622b
I-Chang Liao <31240453+yx1hlqbk@users.noreply.github.com>
2019-05-28 11:04:15 +0800

    Add files via upload

---
+++ Upload.php
@@ -0,0 +1,233 @@
+<?php
+
+/**
+  *
+  */
+class Upload
+{
+    protected $returnMessage = false;
+
+    protected $repeatCount = 50;
+
+    protected $uploadPath = './public/uploads/';
+
+    protected $typeOf = '';
+
+    protected $rename = false;
+
+    protected $fileNameCharacter = false;
+
+    protected $allowedType = '*';
+
+    protected $unallowedType = ['php', 'html', 'xml', 'asp', 'jspx', 'jsp'];
+
+    protected $compress = false;
+
+    protected $compressRatio = 0.5;
+
+    protected $width = '';
+
+    protected $height = '';
+
+    /**
+      * 變數初始設定
+      *
+      * @param array $config
+      */
+    private function init($config)
+    {
+        foreach ($config as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    /**
+      * 執行
+      *
+      * @param string $name
+      * @param array $config
+      */
+    public function run($name = '', $config = [])
+    {
+        $this->init($config);
+
+        if ($this->formUploadCheck($name) !== true) {
+            return $this->getErrorMessage();
+        }
+
+        list($fileName, $fileType) = explode('.', $_FILES[$name]['name']);
+
+        if ($this->fileTypeCheck($fileType) !== true) {
+            return $this->getErrorMessage();
+        }
+
+        $this->uploadPathFolderCheck();
+
+        $this->fileNameCharacterCheck($fileName);
+
+        $fileNameNew = $this->getFileName($fileName, $fileType);
+        $path = $this->uploadPath.$fileNameNew;
+
+        if (is_file($path)) {
+            for ($i=0; $i < $this->repeatCount ; $i++) {
+                $fileNameNew = $this->getFileName($fileName, $fileType, $i);
+                $path = $this->uploadPath.$fileNameNew;
+
+                if (!is_file($path)) {
+                    break;
+                }
+            }
+        }
+
+        if (move_uploaded_file($_FILES[$name]["tmp_name"], $path) === true) {
+            if ($this->compress === true && $this->typeOf === 'img') {
+                $this->compressImg($path, $this->width, $this->height);
+            }
+
+            return $fileNameNew;
+        } else {
+            return $this->getErrorMessage();
+        }
+    }
+
+    /**
+      * 表單檔案上傳資料檢查
+      *
+      * @param string $name
+      */
+    private function formUploadCheck($name)
+    {
+        if (!isset($_FILES[$name])) {
+            return false;
+        }
+
+        if (empty($_FILES[$name]['name'])) {
+            return false;
+        }
+
+        //判斷是否透過Http post上傳
+        if (!is_uploaded_file($_FILES[$name]["tmp_name"])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+      * 檔案副檔名檢查
+      *
+      * @param string $fileType
+      */
+    private function fileTypeCheck($fileType)
+    {
+        if (in_array(strtolower($fileType), $this->unallowedType)) {
+            return false;
+        }
+
+        //判斷是否有自行輸入允許檔案類型
+        if ($this->allowedType !== '*') {
+            if (!in_array(strtolower($fileType), $this->allowedType)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+      * 上傳位置檢查
+      *
+      */
+    private function uploadPathFolderCheck()
+    {
+        if (!is_dir($this->uploadPath)) {
+            mkdir($this->uploadPath, 0777, TRUE);
+        }
+    }
+
+    /**
+      * 檔案字元檢查
+      *
+      */
+    private function fileNameCharacterCheck($fileName)
+    {
+        if (mb_strlen($fileName, mb_detect_encoding($fileName)) != strlen($fileName)) {
+            $this->fileNameCharacter = true;
+        }
+    }
+
+    /**
+      * 檔名命名
+      *
+      * @param string $fileName
+      * @param string $fileType
+      * @param int $num
+      */
+    private function getFileName($fileName, $fileType, $num = '')
+    {
+        $fileName = preg_replace('/-|\*|#/', '', $fileName).$num;
+
+        if ($this->rename === true) {
+            $fileName = date("Ym").rand(0,9).rand(0,9).rand(0,9);
+        } else {
+            if ($this->fileNameCharacter === true) {
+                $fileName = date("Ym").rand(0,9).rand(0,9).rand(0,9);
+            }
+        }
+
+        return $fileName.'.'.strtolower($fileType);
+    }
+
+    /**
+      * 圖片壓縮
+      *
+      * @param string $imgSrc
+      * @param int $requestWidth
+      * @param int $requestHeight
+      */
+    private function compressImg($imgSrc, $requestWidth, $requestHeight)
+    {
+        list($width, $height, $type) = getimagesize($imgSrc); //取檔案資訊
+
+        //image_type_to_extension : 1=gif 2=jpg 3=png 4=swf 5=psd 6=bmp 7=tiff 9=jpc 10=jp2
+        $imageinfo = [
+            'width' => $width,
+            'height' => $height,
+            'type' => image_type_to_extension($type, false) //取檔案類型，不含.(false)
+        ];
+
+        if ($requestWidth == '' && $requestHeight == '') {
+            $requestWidth = $width * $this->compressRatio;
+            $requestHeight = $height * $this->compressRatio;
+        }
+
+        //僅支援格式
+        $allowedCompressimgType = ['gif', 'jpg', 'png', 'bmp', 'jpeg'];
+
+        if (in_array($imageinfo['type'], $allowedCompressimgType)) {
+            $fun = "imagecreatefrom".$imageinfo['type'];
+            $image = $fun($imgSrc);
+            $imageThump = imagecreatetruecolor($requestWidth, $requestHeight);
+            imagecopyresampled($imageThump, $image, 0, 0, 0, 0, $requestWidth, $requestHeight, $imageinfo['width'], $imageinfo['height']);
+            imagedestroy($image);
+            $image = $imageThump;
+            $funcs = "image".$imageinfo['type'];
+            $funcs($image, $imgSrc);
+        }
+    }
+
+    /**
+      * 錯誤訊息
+      *
+      * @param string $number
+      */
+    private function getErrorMessage($number = '')
+    {
+        if ($this->returnMessage === true) {
+            return $number;
+        } else {
+            return '';
+        }
+    }
+
+}
